9d15f2f4b4748b63d517e0b1e736d313c5c3d7f5
Crowdbotics <team@crowdbotics.com>
2024-01-26 22:24:17 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "frauddetectionbank-45624",
+  url: "https://frauddetectionbank-45624.botics.co",
+  api: "https://frauddetectionbank-45624.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
